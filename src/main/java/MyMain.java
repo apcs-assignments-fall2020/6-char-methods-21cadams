9891db48458e895a9909399abf8b75d443a377a5
21cadams <21cadams@athenian.org>
2020-09-22 04:36:54 +0000

    Char Methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -6,35 +6,83 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (ch >= 'A' && ch <= 'Z') {
+            ch += 32; 
+        }
+        else{
+            System.out.println(ch + " is not a letter");
+        }
+        System.out.println("ch is now a lowercase letter " + ch);   
+        return ch;
     }
 
     // Returns a copy of a String, where all letters 
     // are now lowercase
     public static String myToLowerCase(String str) {
-        // REPLACE WITH YOUR CODE
-        return "";
+        int length = str.length();
+        char [] ch = str.toCharArray();
+        String fin = "";
+        for (int i = 0; i < length; i++){
+            if (ch[i] >= 'A' && ch [i] <='Z'){
+                ch[i] += 32; 
+                fin = fin + ch[i];
+            }
+            else{
+                fin = fin + ch[i];
+            }
+        }
+        System.out.println("The sting lowercase is " + fin);
+        return fin;
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not an lowercase letter, 
     // it is returned unchanged. 
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (ch >= 'a' && ch <= 'z') {
+            ch -= 32;
+            
+        }
+        else{
+            System.out.println(ch + " is not a letter");
+        }
+        
+        System.out.println("ch is now an uppercase letter " + ch);   
+        return ch;
     }
 
     // Returns a copy of a String, where all letters 
     // are now uppercase
     public static String myToUpperCase(String str) {
-        // REPLACE WITH YOUR CODE HERE
-        return "";
+        int lengthh = str.length();
+        char[] ch2 = str.toCharArray();
+        String fin= "";
+        for (int i = 0; i < lengthh; i++){
+            if (ch2[i] >= 'a' && ch2 [i] <='z'){
+                ch2[i] -= 32; 
+                fin = fin + ch2[i];
+            }
+            else{
+                fin = fin + ch2[i];
+            }
+        }
+        System.out.println("The sting uppercase is " + fin);
+        return fin;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a string"); 
+        String str = sc.nextLine(); 
+        System.out.println("would you like to make the string uppercase or lowercase?");
+        String uorl = sc.nextLine();
+        System.out.println(uorl);
+        if (uorl == "uppercase"){
+            myToUpperCase(str);
+        } else if(uorl == "lowercase"){
+            myToLowerCase(str);
+        }
+        sc.close();
     }
 }
